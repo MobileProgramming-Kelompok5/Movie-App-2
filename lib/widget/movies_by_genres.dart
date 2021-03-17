654bb94507f6b952d98d2c963293ba5ce2338fc5
Cordellya <dellya110701@gmail.com>
2021-03-17 17:53:53 +0700

    tab genre done

--- lib/widget/movies_by_genres.dart
+++ lib/widget/movies_by_genres.dart
@@ -1,12 +1,15 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:movie_app_3/bloc/get_genres_bloc.dart';
 import 'package:movie_app_3/bloc/get_movies_bloc.dart';
 import 'package:movie_app_3/bloc/get_movies_byGenre_bloc.dart';
 import 'package:movie_app_3/model/movie.dart';
 import 'package:movie_app_3/model/movie_response.dart';
 // import 'package:movie_app_3/screens/detail_screen.dart';
 import 'package:movie_app_3/style/theme.dart' as Style;
+import 'package:movie_app_3/widget/movie_card.dart';
 
 class GenreMovies extends StatefulWidget {
   final int genreId;
@@ -16,12 +19,24 @@ class GenreMovies extends StatefulWidget {
 }
 
 class _GenreMoviesState extends State<GenreMovies> {
+  PageController _pageController;
+  int initialPage = 1;
   final int genreId;
   _GenreMoviesState(this.genreId);
   @override
   void initState() {
     super.initState();
     moviesByGenreBloc..getMoviesByGenre(genreId);
+    _pageController = PageController(
+      viewportFraction: 0.8,
+      initialPage: initialPage,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
   }
 
   @override
@@ -91,116 +106,48 @@ class _GenreMoviesState extends State<GenreMovies> {
         ),
       );
     } else
-      return Container(
-        height: 270.0,
-        padding: EdgeInsets.only(left: 10.0),
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: movies.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.only(top: 10.0, bottom: 10.0, right: 15.0),
-              child: GestureDetector(
-                onTap: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //     builder: (context) => MovieDetailScreen(movie: movies[index]),
-                  //   ),
-                  // );
-                },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    movies[index].poster == null
-                        ? Container(
-                            width: 120.0,
-                            height: 180.0,
-                            decoration: new BoxDecoration(
-                              color: Style.Colors.secondColor,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(2.0)),
-                              shape: BoxShape.rectangle,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(
-                                  EvaIcons.filmOutline,
-                                  color: Colors.white,
-                                  size: 60.0,
-                                )
-                              ],
-                            ),
-                          )
-                        : Container(
-                            width: 120.0,
-                            height: 180.0,
-                            decoration: new BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(2.0)),
-                              shape: BoxShape.rectangle,
-                              image: new DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: NetworkImage(
-                                      "https://image.tmdb.org/t/p/w200/" +
-                                          movies[index].poster)),
-                            )),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      width: 100,
-                      child: Text(
-                        movies[index].title,
-                        maxLines: 2,
-                        style: TextStyle(
-                            height: 1.4,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 11.0),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          movies[index].rating.toString(),
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 10.0,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          width: 5.0,
-                        ),
-//                         RatingBar(
-//                       itemSize: 8.0,
-//    initialRating: movies[index].rating / 2,
-//    minRating: 1,
-//    direction: Axis.horizontal,
-//    allowHalfRating: true,
-//    itemCount: 5,
-//    itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
-//    itemBuilder: (context, _) => Icon(
-//      EvaIcons.star,
-//      color: Style.Colors.secondColor,
-//    ),
-//    onRatingUpdate: (rating) {
-//      print(rating);
-//    },
-// )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            );
-          },
+      return Padding(
+        padding: EdgeInsets.symmetric(vertical: 50),
+        child: AspectRatio(
+          aspectRatio: 0.85,
+          child: PageView.builder(
+            onPageChanged: (value) {
+              setState(() {
+                initialPage = value;
+              });
+            },
+            controller: _pageController,
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              return buildMovieSlider(movies, index);
+//
+            },
+          ),
         ),
       );
+  }
+
+  Widget buildMovieSlider(List<Movie> movies, int index) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (context, child) {
+        double value = 0;
+        if (_pageController.position.haveDimensions) {
+          value = index - _pageController.page;
+          value = (value * 0.038).clamp(-1, 1);
+        }
+        return AnimatedOpacity(
+          duration: Duration(milliseconds: 350),
+          opacity: initialPage == index ? 1 : 0.4,
+          child: Transform.rotate(
+            angle: math.pi * value,
+            child: MovieCard(
+              movies: movies[index],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
